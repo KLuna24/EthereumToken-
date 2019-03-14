1e45cb0302f7d4bd2c540a860f8eb6d5ee9ab0c3
Kevin Luna <39965672+KLuna24@users.noreply.github.com>
2019-03-13 22:57:50 -0400

    Add files via upload

---
+++ ExampleTokenCrowdsale.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.24;
+
+
+import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
+
+contract ExampleTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale{
+
+	//minimum investor Contribution - at least 2.5
+	//minimum investor Contribution - 50000000000000000000
+	uint256 public investorMinCap = 50000000000000000000;
+	uint256 public investorHardCap = 50000000000000000000;
+
+	mapping(address => uint256) public contributions;
+
+	constructor(uint256 _rate,
+	  address _wallet,
+	  ERC20 _token,
+	  uint256 _cap)
+	Crowdsale(_rate, _wallet, _token)
+	CappedCrowdsale(_cap)
+	public{
+  //function getTokensLeft() public view returns (uint256){
+           //return cap-weiRaised;
+      // }
+	}
+
+
+  function _preValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+  {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+    uint256 _existingContribution = contributions[_beneficiary];
+    uint256 _newContribution = _existingContribution.add(_weiAmount);
+    require(_newContribution >= investorMinCap && _newContribution <= investorHardCap);
+	contributions[_beneficiary] = _newContribution;
+  }
+  function getTokensLeft() public view returns (uint256){
+       return cap-weiRaised;
+  }
+
+}
